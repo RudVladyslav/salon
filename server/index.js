358a44646bff2ce9785b7f344273c4966afc8d50
RudVladyslav <gydinidog74@gmail.com>
2023-01-10 18:38:12 +0200

    add routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import sequelize from './db.js';
+import router from './routes/index.js';
+import models from './models/models.js';
+
 dotenv.config();
 
 const app = express();
@@ -8,9 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(PORT, '192.168.0.102', () => console.log(
         `Server has been started on port:${PORT} \n http://localhost:${PORT}/ \n or http://192.168.0.103:7200 `));
   } catch (e) {
@@ -18,4 +25,4 @@ const start = async () => {
   }
 };
 
-start()
+start();
